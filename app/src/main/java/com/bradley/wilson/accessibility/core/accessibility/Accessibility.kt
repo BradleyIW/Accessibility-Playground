475c600f6299406e2b2b6a97356bfbfaedbb61bf
Bradley Wilson <bradley.wilson@wire.com>
2020-07-09 14:03:56 +0200

    removed redundant cal for isTouchExplorationEnabled()

--- app/src/main/java/com/bradley/wilson/accessibility/core/accessibility/Accessibility.kt
+++ app/src/main/java/com/bradley/wilson/accessibility/core/accessibility/Accessibility.kt
@@ -5,7 +5,7 @@ import android.view.accessibility.AccessibilityManager
 
 class Accessibility(private val am: AccessibilityManager) {
 
-    fun isTalkbackEnabled() = am.isEnabled && am.isTouchExplorationEnabled &&
+    fun isTalkbackEnabled() = am.isEnabled &&
             am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_SPOKEN)
                 .isNotEmpty()
 

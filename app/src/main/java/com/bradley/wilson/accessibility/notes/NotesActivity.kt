f5349c63614486c3ce6ea1c427346f2f2ed7e76a
Bradley Wilson <bradley.wilson@wire.com>
2020-07-09 14:23:49 +0200

    removed else closure for accessibility users

--- app/src/main/java/com/bradley/wilson/accessibility/notes/NotesActivity.kt
+++ app/src/main/java/com/bradley/wilson/accessibility/notes/NotesActivity.kt
@@ -55,8 +55,6 @@ class NotesActivity : AppCompatActivity(R.layout.activity_notes) {
         if (!accessibility.isTalkbackEnabled()) {
             view.requestFocus()
             showKeyboard()
-        } else {
-            clearFocusAndHideKeyboard(view)
         }
     }
 
